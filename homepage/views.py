03414583aed106e476dbe468585cd580f5bb57d7
Hantsi <hantsitaumang@gmail.com>
2022-06-07 21:50:29 +0200

    email

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import redirect, render
 from .forms import messageform
-from django.core.mail import EmailMessage
-from django.conf import settings
-from django.template.loader import render_to_string
 # Create your views here.
 
 def index(request):
@@ -10,15 +7,6 @@ def index(request):
         form = messageform(request.POST)
         if form.is_valid():
             form.save()
-            template_admin = render_to_string('templates/info_email.html')
-            email_admin = EmailMessage(
-                'Message',
-                template_admin,
-                settings.EMAIL_HOST_USER,
-                ['hantsitaumang@gmail.com']
-            )
-            email_admin.fail_silently = False
-            email_admin.send()
             return redirect('sent')
         else:
             return redirect('/')
